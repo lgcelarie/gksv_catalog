ef387a18d37dd1ef0224a590a89db37d00b7699a
Guillermo Celarie <lg.celarie@gmail.com>
2018-11-14 22:03:39 -0600

    cambios en modelos, templates placeholders

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-from flask-moment import Moment
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
--- app/models.py
+++ app/models.py
@@ -1,1 +1,15 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from SQLAlchemy
+
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100))
+    estado = db.Column(db.Enum({}))
+    cant_visto = db.Column(db.Integer)
+    peq_desc = db.Column(sb.String(244))
+    descripcion = db.Column()
+    marca_id = db.Column()
+
+
