19ee699d2f8df549d58bdf7db41b638fc18c9d6f
Guillermo Celarie <lg.celarie@gmail.com>
2018-11-15 22:06:21 -0600

    ajustes para reconocer modelos, migraciones basicas

--- app/models.py
+++ app/models.py
@@ -1,15 +1,50 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from SQLAlchemy
+from . import db
 
+
+class Marca(db.Model):
+    __tablename__ = 'marcas'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), index = True)
+    productos = db.relationship('Producto', backref='marca', lazy='dynamic')
+    def __repr__(self):
+        return '<Marca %r>' % self.id
+
+
+class Imagen(db.Model):
+    __tablename__ = 'imagenes'
+    id = db.Column(db.Integer, primary_key = True)
+    primaria = db.Column(db.Boolean)
+    ruta = db.Column(db.String(120))
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'))
+
+    def __repr__(self):
+        return '<Imagen %r>' % self.id
+
+
+
+class Visita(db.Model):
+    __tablename__ = 'visitas'
+    id = db.Column(db.Integer, primary_key = True)
+    ip = db.Column(db.String(15))
+    fecha = db.Column(db.DateTime)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'))
+
+    def __repr__(self):
+        return '<Visita %r>' % self.id
+        
 
 class Producto(db.Model):
     __tablename__ = 'productos'
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(100))
-    estado = db.Column(db.Enum({}))
+    estado = db.Column(db.Enum(('Disponible', 'Reservado', 'Agotado')))
     cant_visto = db.Column(db.Integer)
-    peq_desc = db.Column(sb.String(244))
-    descripcion = db.Column()
-    marca_id = db.Column()
+    peq_desc = db.Column(db.String(250))
+    descripcion = db.Column(db.Text)
+    marca_id = db.Column(db.Integer, db.ForeignKey('marcas.id'))
+    imagenes = db.relationship('Imagen', backref='producto', lazy='dynamic')
+    visitas = db.relationship('Visita', backref='producto', lazy='dynamic')
 
-
+    def __repr__(self):
+        return '<Producto %r>' % self.id
--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Role
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+from app import models
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -12,7 +12,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
