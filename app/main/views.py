86dbd9f0b9c523154c5b5847539505315a09fc88
Guillermo Celarie <lgcelarie@Guillermos-MacBook-Pro.local>
2018-12-16 22:10:40 -0600

    -vista para cargar archivo csv fijo con catalogo. -imagenes finales para marcas -logo invertido para footer

--- app/main/views.py
+++ app/main/views.py
@@ -84,3 +84,40 @@ def search():
     #     if page > 1 else None
     return render_template('search.html', title='Search', productos=results,
                            )
+
+@main.route('/load')
+def cargar_productos():
+    import csv
+    from ..models import Imagen
+    file = '/Users/lgcelarie/Documents/flask_cat/gksv_catalog/app/static/cat_pagina.csv'
+
+    with open(file) as csvfile:
+        lector = csv.DictReader(csvfile)
+        slug_list = list()
+        for row in lector:
+            print(row)
+            producto = Producto()
+            producto.id = row['id']
+            producto.nombre = row['nombre']
+            producto.slug = row['nombre'].replace(' ','-')
+            if producto.slug in slug_list:
+                producto.slug += str(row['id'])
+            slug_list.append(producto.slug)
+            producto.descripcion = row['descripcion']
+            producto.peq_desc = row['descripcion'][:150]
+            producto.marca_id = row['marca']
+            producto.estado = row['estado']
+            producto.condicion = float(row['condicion'])
+            producto.cant_visto = 0
+            db.session.add(producto)
+
+            for i in range(1,7):
+                imagen = Imagen()
+                if i == 1:
+                    imagen.primaria = 1
+                else:
+                    imagen.primaria = 0
+                imagen.ruta = row['id'] + '_' + str(i) + '.jpg'
+                producto.imagenes.append(imagen)
+                db.session.add(imagen)
+        db.session.commit()
